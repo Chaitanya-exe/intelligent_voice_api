85c208bd0b72b836eac92400ceda4be8a0518ce7
Chaitanya-exe <jeetkrishna3333@gmail.com>
2026-03-07 17:19:38 +0530

    stt pipeline && minimal conversational loop

---
+++ ears/eardrum.py
@@ -0,0 +1,40 @@
+from faster_whisper import WhisperModel
+import sounddevice as sd
+import numpy as np
+
+class STTPipeline:
+    def __init__(self):
+        self.ears = WhisperModel("small", device="auto", compute_type="int8")
+
+    def _record_audio(self, seconds:int = 4, samplerate: int = 16000):
+
+        print("Please start speaking...")
+
+        audio = sd.rec(
+            int(seconds * samplerate),
+            samplerate=samplerate,
+            channels=1,
+            dtype='float32'
+        )
+
+        sd.wait()
+
+        audio = np.squeeze(audio)
+
+        return audio
+    
+    def get_transcription(self):
+        audio = self._record_audio()
+
+        segments, info = self.ears.transcribe(
+            audio=audio,
+            beam_size=1,
+            language='en',
+            condition_on_previous_text=False,
+            vad_filter=True
+        )
+
+        text = " ".join(seg.text for seg in segments)
+        print("Detected language: ", info.language)
+        print("User: ", text)
+        return text
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from langchain_core.messages import HumanMessage, SystemMessage
 from brain.brain_voice import BrainVoice
+from ears.eardrum import STTPipeline
 
 tts = BrainVoice()
-
+stt = STTPipeline()
 system_prompt = """
 You are a voice assisstant who talks in hindi, your task is make simple conversations based on user input. Your text output will be used in text-to-speech engine, so it is neccessary to produce correct hindi text with proper punctuations according to the conversation, for native english words that don't have a translation in hindi produce text, pronounced same as english when spoken
 """
@@ -11,8 +12,10 @@ def main():
     tts.start()
     while True:
 
-        user = input("user: ")
-
+        input("Press enter to record your message: ")
+        
+        user = stt.get_transcription()
+        
         conversation = [
             SystemMessage(content=system_prompt),
             HumanMessage(content=user)
