aaaa60165a2a8e85f7aef20a5942496e06659ff8
Chaitanya-exe <jeetkrishna3333@gmail.com>
2026-03-09 23:16:33 +0530

    conversation controller module init

--- ears/eardrum.py
+++ ears/eardrum.py
@@ -1,46 +1,28 @@
 from faster_whisper import WhisperModel
-import sounddevice as sd
-import numpy as np
 from queue import Queue
 from threading import Thread
 
 class EarDrum:
-    def __init__(self):
-        self.ears = WhisperModel("small", device="auto", compute_type="int8")
-        self.mic_q = Queue()
+    def __init__(self, speech_q: Queue, text_q: Queue):
+        self.model = WhisperModel("small", device="auto", compute_type="int8")
+        self.speech_q = speech_q
+        self.text_q = text_q
 
-    def _record_audio(self, seconds:int = 4, samplerate: int = 16000):
+    def worker(self):
 
-        print("Please start speaking...")
+        while True:
 
-        audio = sd.rec(
-            int(seconds * samplerate),
-            samplerate=samplerate,
-            channels=1,
-            dtype='float32'
-        )
+            audio = self.speech_q.get()
 
-        sd.wait()
+            segments, _ = self.model.transcribe(
+                audio,
+                beam_size=1,
+                language="en",
+                condition_on_previous_text=False
+            )
 
-        audio = np.squeeze(audio)
+            text = " ".join(seg.text for seg in segments)
 
-        return audio
-    
-    def get_transcription(self):
-        audio = self._record_audio()
+            self.text_q.put(text)
 
-        segments, info = self.ears.transcribe(
-            audio=audio,
-            beam_size=1,
-            language='en',
-            condition_on_previous_text=False,
-            vad_filter=True
-        )
-
-        text = " ".join(seg.text for seg in segments)
-        print("Detected language: ", info.language)
-        print("User: ", text)
-        return text
-    
-    def start(self):
-        pass
+            self.speech_q.task_done()
---
+++ vad/vad_pipe.py
@@ -0,0 +1,43 @@
+import sounddevice as sd
+import numpy as np
+from silero_vad import load_silero_vad
+from silero_vad.utils_vad import VADIterator
+import threading
+from queue import Queue
+
+class VadPipeline:
+    def __init__(self, speech_q: Queue, samplerate:int = 16000):
+        self.samplerate = samplerate
+        self.q = speech_q
+        self.model = load_silero_vad()
+        self.vad = VADIterator(model=self.model)
+        self.buffer = []
+
+    def process_audio(self, audio):
+        speech = self.vad(audio)
+
+        if speech:
+            self.buffer.append(speech)
+        else:
+            if len(self.buffer) > 1:
+                segment = np.concatenate(self.buffer)
+                self.q.put(segment)
+                self.buffer = []
+    
+    def start(self):
+        def callback(indata, frames, time, status):
+            audio = indata[:,0]
+            self.process_audio(audio)
+        
+        stream = sd.InputStream(
+            samplerate=self.samplerate,
+            blocksize=512,
+            channels=1,
+            callback=callback
+        )
+
+        stream.start()
+        print("Listening...")
+
+        while True:
+            sd.sleep(1000)
