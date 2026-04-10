8fc45bdb24a008f5b35b5a0e79cdce6c7fe97322
Chaitanya-exe <jeetkrishna3333@gmail.com>
2026-04-11 00:41:45 +0530

    tts refactor

--- ai_pipeline/brain_voice.py
+++ ai_pipeline/brain_voice.py
@@ -6,8 +6,11 @@ from kokoro import KPipeline
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_ollama import ChatOllama
+import requests
 import time
 from ai_pipeline.controller import ConversationController
+import os
+import numpy as np
 
 class BrainVoice:
 
@@ -17,7 +20,8 @@ class BrainVoice:
     MAX_CHARS = 60
     MIN_WORDS = 3
     MAX_LATENCY = 0.4
-
+    TTS_API = os.getenv("TTS_API","")
+    TTS_URL = "https://yourvoic.com/api/v1/tts/stream"
     def __init__(self, text_q: Queue, controller: ConversationController):
         self.voice = KPipeline(lang_code='h', repo_id='hexgrad/Kokoro-82M')
         self.model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
@@ -27,8 +31,8 @@ Your name is Anushka and you have a female persona.
 You are a professional AI sales agent making cold calls to potential customers.
 Your goal is to have a natural, human-like conversation and introduce a product or service in a polite and engaging way.
 Strict rules:
-- Speak only in Hindi.
-- Use simple, conversational Hindi (natural spoken language).
+- Speak in Hinglish, which is a combination of hindi and english.
+- Use simple, conversational Hindi and some english only for conversation (natural spoken language).
 - Keep sentences short and easy to speak.
 - Use proper punctuation (। ? ! ,) to create natural pauses.
 - Do not speak too fast or produce long paragraphs.
@@ -78,6 +82,38 @@ You are speaking over a call, not writing text.
     
     def is_valid_chunk(self, chunk):
         return len(chunk.split(" ")) >= self.MIN_WORDS
+
+    def yield_audio(self, text, voice, speed):
+        response = requests.post(
+            url=self.TTS_URL,
+            headers={
+                "X-API-Key": self.TTS_API,
+                "Content-Type": "application/json",
+                "Accept": "audio/pcm"
+            },
+            json={
+                "text": text,
+                "voice": voice,
+                "speed": speed,
+                "language": "hi",
+                "model": "aura-lite",
+                "pitch": 1
+            },
+            stream=True
+        )
+
+        for chunk in response.iter_content(chunk_size=4096):
+
+            if not chunk:
+                continue
+
+            if len(chunk) % 2 != 0:
+                continue
+
+            audio = np.frombuffer(chunk, dtype=np.int16)
+            audio = audio.astype(np.float32) / 32768.0
+
+            yield audio
 
     def llm_worker(self):
         while True:
@@ -136,6 +172,7 @@ You are speaking over a call, not writing text.
                 self.text_q.task_done()
             except Exception as e:
                 print("Error occured: ", str(e))
+                continue
         
     def tts_worker(self):
         while True:
@@ -147,7 +184,8 @@ You are speaking over a call, not writing text.
             if not self.controller.ai_speaking:
                 self.controller.start_ai()
 
-            generator = self.voice(text=text, voice='hf_alpha', speed=1.3)
+            generator_ = self.yield_audio(text=text, voice='Nisha', speed=1.3)
+            generator = self.voice(text=text, voice='hf_alpha', speed=1.6)
 
             for _, _, audio in generator:
                 if self.controller.should_interrupt():
@@ -158,6 +196,8 @@ You are speaking over a call, not writing text.
                         except:
                             break
                     break
+
+                
                 self.speaker_stream.write(audio)
             
             self.controller.stop_ai()
